8ba2b20303c5e059b86e93acda2a75c330316546
ItWarior <Tarasbennet@gmail.com>
2021-07-09 23:07:49 +0300

    Server

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-let ownError = require('./three');
-try {
-   throw new ownError('AAAAAAA', "code","desc");
-} catch (e) {
-   e.pretty();
-};
+let http = require('http');   //підключаємо модуль
+let fs = require('fs');      
+
+let server = http.createServer();
+server.on('request', function (request, response) {//навішуємо на сервер подію, тобто
+   response.writeHead(200, {
+      'Content-Type':'text/html'
+   })
+   fs.readFile('./index.html', function (error, data) {
+      response.write(data.toString());
+      response.end();    //коли буде запит reques викликаємо collback
+   })
+})                                            //який повертає response
+server.listen(3000, function () {        //вказуємо який порт буде слухати сервер
+   console.log("listen...");             //завжди вказуємо 3000 порт
+})
